df8823008d0bc3284d62a36dc0bd11bf6a50493a
Dreymeister <drey.owen@gmail.com>
2023-05-03 23:17:49 -0400

    Reverted MakePassword

--- src/makepassword.js
+++ src/makepassword.js
@@ -1,21 +1,23 @@
-//function corrected, fixed
 'use strict'
+
 const fs = require('fs');
-const {readFile, writeFile, hash} = require('./utility')
+const {readFile, hash} = require('./utility');
 
 function makepassword(passwordFileName, passwordEncFileName) {
-    
   const inputFromFile = readFile(passwordFileName);
-    const emailPasswordHashedPair = inputFromFile.map(line => {
-      const [email, password] = line.split(":");
-      return `${email}:${hash(password)}`;
-    });
-   
-    writeFile(emailPasswordHashedPair, passwordEncFileName);
-  }
-  
-if (require.main === module) {
-    makepassword('./password.txt', './password.enc.txt')
+  const emailPasswordHashedPair = inputFromFile.map(line => {
+    const [email, password] = line.split(":");
+    return `${email}:${hash(password)}`;
+  });
+
+  fs.unlinkSync(passwordEncFileName); // remove the file if it exists
+  emailPasswordHashedPair.forEach(pair => {
+    fs.appendFileSync(passwordEncFileName, pair + '\n');
+  });
 }
 
-module.exports = {makepassword};
+module.exports = { makepassword };
+
+
+// This section of code is causing the error, after debbuging and testing constantly, the file will not create. This is the portion that is
+// failing my acceptence tests. Please help! I dont want to make it too complex, keep it dumb and simple is what I want to do!
