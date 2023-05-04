db0bbc1882fca4525acd3b5b11cadd4404087245
Dreymeister <drey.owen@gmail.com>
2023-05-03 21:29:09 -0400

    Revamped MakePassword Test File

--- tests/makepassword.test.js
+++ tests/makepassword.test.js
@@ -1,56 +1,34 @@
-// To unit-test the function that generates 'password.enc.txt', you should
-//  make sure the unit test does the following check.
-// 1. Make sure password.enc.txt does not exist before running the function.
-// 2. Make sure password.enc.txt does exist after running the function.
-// 3. Make sure the contents of password.enc.txt has correct contents.
-// For unit tests, you don't have to have a large input in the beginning.
-// Start with smallest input, and add more contents in the input
-
-/*
-// Let's say you have a toHash() function in this module
-
-test('Check toHash(): if the email:password is converted into email:hashPassword', () => {
-    const input = ???
-    const output = ???
-    expect(p.toHash(input)).toBe(output);
-});
-*/
 
 
 const p = require('../src/makepassword');
 const u = require('../src/utility');
 const fs = require('fs');
-const util = require('../src/utility');
+const util = require("../src/utility");
+
 
 describe("makepassword should create file", () => {
-    const fileName = './tests/passwordtest.txt'
-    const encFileName = './tests/passwordtest.enc.txt'
+    const fileName = "./tests/passwordtest.txt";
+    const encFileName = "./tests/passwordtest.enc.txt";
+  
+    test("create password.enc.txt", () => {
+      // Make sure password.enc.txt does not exist before running the function
+      expect(fs.existsSync(encFileName)).toBe(false);
+  
+      // Run makepassword to create password.enc.txt
+      p.makepassword(fileName, encFileName);
+  
+      // Make sure password.enc.txt exists after running the function.
+      expect(fs.existsSync(encFileName)).toBe(true);
+  
+      // Only returning false?
+      const inputFromFile = util.readFile(`./${encFileName}`);
+      const inputFromFileComp = util.readFile(`./${fileName}`);
+      const [email, plainPassword] = inputFromFileComp[0].split(':');
+      const hashedPassword = util.hash(plainPassword);
+      const [emailIn, passwordIn] = inputFromFile[0].split(':');
 
-    // 1. Check if password.enc.txt does not exist before running the function.
-    test('Check if password.enc.txt does not exist', () => {
-        if (fs.existsSync(encFileName)) {
-            fs.unlinkSync(encFileName);
-        }
-        expect(fs.existsSync(encFileName)).toBe(false);
-
-        // 2. Run makepassword to create the encrypted file.
-        p.makepassword(fileName, encFileName);
-
-        // 3. Make sure password.enc.txt does exist after running the function.
-        expect(fs.existsSync(encFileName)).toBe(true);
-
-        // 4. Make sure the contents of password.enc.txt has correct contents.
-        const [emailFromFile, hashedPasswordFromFile] = util
-            .readFile(encFileName)[0]
-            .split(':');
-        const [emailAndPasswordFromFile] = util
-            .readFile(fileName)
-            .split('\n')
-            .filter((line) => line.includes(emailFromFile));
-        const [emailIn, passwordIn] = emailAndPasswordFromFile.split(':');
-        const passwordInHashed = util.hash(passwordIn);
-          
-        expect(emailIn).toBe(emailFromFile);
-        expect(passwordInHashed).toBe(hashedPasswordFromFile);
+        expect(emailIn).toBe(email);
+        expect(passwordIn).toBe(hashedPassword);
     });
-});
+  });
+
